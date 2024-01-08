6eb55d7b821c477be8f11e00b4d5408e8dd98ec2
miksor2023 <miksor@yandex.ru>
2024-01-08 19:47:26 +0300

    intial commit

---
+++ src/main/java/com/pro/sky/course2/hw080124/Hw080124Application.java
@@ -0,0 +1,13 @@
+package com.pro.sky.course2.hw080124;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw080124Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw080124Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/sky/course2/hw080124/Hw080124ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.sky.course2.hw080124;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw080124ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
